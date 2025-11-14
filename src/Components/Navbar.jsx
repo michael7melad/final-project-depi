1344616594fdd867b07bdcb80c2e2bc1aaae4395
michael <michael7melad@gmail.com>
2025-11-14 17:46:27 +0200

    Add NavBar

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,3 +1,5 @@
+// NavBar
+
 import React, { useState, useEffect } from "react";
 import { Link, useLocation, useNavigate } from "react-router-dom";
 import { motion, AnimatePresence } from "framer-motion";
